adf3c97b50326f34bcc32e6632cd2e1612dca8bc
Hancy Wang <46825897+hancygit@users.noreply.github.com>
2020-04-20 01:09:36 -0700

    make it more clear about efficiency!

--- src/component/FormTitle.js
+++ src/component/FormTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FormTitle(props) {
     return (
         <h4 className="text-center">
-            Let's evaluate your meeting with Machine Learning
+            Let's evaluate your meeting efficiency with Machine Learning Model! 
         </h4>
     );
 }
