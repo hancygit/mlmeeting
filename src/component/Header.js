a7e8920dfaebe6581ffc2bd418a27ac64128c40c
Hancy Wang <46825897+hancygit@users.noreply.github.com>
2020-04-20 01:21:46 -0700

    fix the syntax error to add a new line

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,10 +3,8 @@ import React from 'react';
 function Header() {
     return (
         <h3 style={{padding: 40, textAlign: 'center'}}>
-            Welcome to this capstone project of MSCSE Spring 2020 class in
-        </h3>
-        <h3 style={{padding: 40, textAlign: 'center'}}> 
-        Whitewater University of California
+            Welcome to this capstone project of MSCSE Spring 2020 class in <br />
+            Whitewater University of California! 
         </h3>
     );
 }
