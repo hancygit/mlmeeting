e8bf1d68f4ba35c8199935b7705d5e20b914b6a3
Hancy Wang <46825897+hancygit@users.noreply.github.com>
2020-04-20 01:06:52 -0700

    update it to be more precisely to describe the project
    It's a capstone project of MSCSE Spring 2020 class.

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <h3 style={{padding: 40, textAlign: 'center'}}>
-            Welcome to Whitewater University of California
+            Welcome to this capstone project of MSCSE Spring 2020 class in Whitewater University of California
         </h3>
     );
 }
