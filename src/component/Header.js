e56f359398e3ce55a37c09178c38f791b4443eb0
Hancy Wang <46825897+hancygit@users.noreply.github.com>
2020-04-20 01:12:30 -0700

    better layout

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Header() {
     return (
         <h3 style={{padding: 40, textAlign: 'center'}}>
-            Welcome to this capstone project of MSCSE Spring 2020 class in Whitewater University of California
+            Welcome to this capstone project of MSCSE Spring 2020 class in
+        </h3>
+        <h3 style={{padding: 40, textAlign: 'center'}}> 
+        Whitewater University of California
         </h3>
     );
 }
